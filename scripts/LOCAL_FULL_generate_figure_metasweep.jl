dc01052f1680e16d7f36f0c4aead824f5d54d845
Graham Smith <grahamas@gmail.com>
2020-10-10 12:51:32 -0500

    Various scripts for generating different figures

---
+++ scripts/LOCAL_FULL_generate_figure_metasweep.jl
@@ -0,0 +1,8 @@
+using TravelingWaveSimulationsPlotting, TravelingWaveSimulations
+
+metasweep_A_fpath = "/home/graham/data/ring_blocking/depblock_A/2020-10-06T22:03:12.176_v1.0-304-g11c6074_dirty"
+
+# Definitely propagating
+save_metasweep(metasweep_A_fpath, (:Aei, :Aee), :blocking_θI, 
+                            :has_propagation)
+                           
