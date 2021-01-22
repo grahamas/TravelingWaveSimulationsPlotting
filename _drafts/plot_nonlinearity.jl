5ba669c6029b9a24901eb688a2457b462a632fda
Graham Smith <grahamas@gmail.com>
2021-01-22 14:40:13 -0700

    Plotting examples and nonlinearities

---
+++ _drafts/plot_nonlinearity.jl
@@ -0,0 +1,114 @@
+using Simulation73, NeuralModels
+using NeuralModels: AbstractSigmoidNonlinearityParameter, AbstractDifferenceOfSigmoidsParameter
+
+using AbstractPlotting
+
+TOL = 0.001
+
+function _bounds_fns(nonl::NeuralModels.NonlinearityParameterWrapperAction, args...)
+    _bounds_fns(nonl.p, args...)
+end
+
+function _bounds_fns(nonl::AbstractSigmoidNonlinearityParameter, tol=TOL)
+    left = (xs) -> findfirst(xs .> tol)
+    right = (xs) -> findfirst(xs .> (1.0 - tol))
+    return (left, right)
+end
+
+function _bounds_fns(nonl::Union{AbstractDifferenceOfSigmoidsParameter,AbstractDifferenceOfSigmoidsAction}, tol=TOL)
+    left = (xs) -> findfirst(xs .> tol)
+    right = (xs) -> findfirst(xs[left(xs):end] .< tol)
+    return (left, right)
+end
+
+function _auto_range_idx!(output, nonl::AbstractNonlinearityAction)
+    (left_fn, right_fn) = _bounds_fns(nonl)
+    left_idx, right_idx = (left_fn(output), right_fn(output))
+    @show right_idx
+    # TODO error messages
+    # left_idx = left_idx isa Int ? max(left_idx-20,firstindex(output)) : firstindex(output)
+    left_idx = firstindex(output)
+    right_idx = right_idx isa Int ? min(right_idx+((right_idx - left_idx)÷4), lastindex(output)) : lastindex(output)
+    return (left_idx:right_idx)
+end
+
+function _auto_range_idx!(outputs::AbstractArray{<:AbstractArray}, ps::AbstractArray)
+    all_idxs = _auto_range_idx!.(outputs, ps)
+    firsts = [idx[begin] for idx in all_idxs]
+    lasts = [idx[end] for idx in all_idxs]
+    @show lasts
+    return (minimum(firsts):maximum(lasts))
+end
+
+_potential_range(::AbstractNonlinearityParameter) = 0.:0.01:100.
+function _potential_range(sig::AbstractSigmoidNonlinearityParameter)
+    @assert sig.a > 0
+    nearly_one = NeuralModels.inverse_simple_sigmoid_fn(0.999, sig.a, sig.θ)
+    radius = nearly_one - sig.θ
+    return range(sig.θ-radius, stop=nearly_one, length=100)
+end
+function _potential_range(dos::AbstractDifferenceOfSigmoidsParameter)
+    @assert get_firing_sigmoid(dos).θ < get_blocking_sigmoid(dos).θ
+    firing_range = _potential_range(get_firing_sigmoid(dos))
+    blocking_range = _potential_range(get_blocking_sigmoid(dos))
+    return range(firing_range[begin], stop=blocking_range[end], length=1000)
+end
+function _potential_range(arr::AbstractArray{<:AbstractNonlinearityParameter})
+    ranges = _potential_range.(arr)
+    left = minimum(first.(ranges))
+    right = maximum(last.(ranges))
+    return range(left, stop=right, length=1000)
+end
+
+function _auto_range(nonl_param::AbstractNonlinearityParameter, potential_range=_potential_range(nonl_param))
+    output = copy(potential_range)
+    dummy_space = CompactLattice(0.0, 1.0, length(potential_range))
+    nonl = nonl_param(dummy_space)
+    nonl(output, nothing, nothing)
+    idxs = _auto_range_idx!(output, nonl)
+    (potential_range[idxs], output[idxs])
+end
+    
+function _auto_range(nonl_params::AbstractArray{<:AbstractNonlinearityParameter}, potential_range=_potential_range(nonl_params))
+    dummy_space = CompactLattice(first(potential_range), last(potential_range), length(potential_range))
+    nonls = map(np -> np(dummy_space), nonl_params)
+    max_outputs = [nonl(collect(potential_range), nothing, nothing) for nonl in nonls]
+    idxs = _auto_range_idx!(max_outputs, nonls)
+    @show idxs
+    ([potential_range[idxs] for _ in max_outputs], [output[idxs] for output in max_outputs])
+end    
+
+function convert_arguments(nonl::AbstractNonlinearityParameter)
+    _auto_range(nonl)
+end
+
+function AbstractPlotting.convert_arguments(P::Type{<:AbstractPlot}, pops::Simulation73.AbstractPOneD{NPOPS,<:AbstractNonlinearityParameter}) where NPOPS
+    return _auto_range(Simulation73.array(pops))
+end
+
+plot_nonlinearity!(scene::Scene, sim::Simulation, args...; kwargs...) = plot_nonlinearity!(scene, sim.model, args...; kwargs...)
+
+plot_nonlinearity!(scene::Scene, model::AbstractModel, args...; kwargs...) = plot_nonlinearity!(scene, model.nonlinearity, args...; pop_names=model.pop_names, kwargs...)
+
+using Colors
+function plot_nonlinearity!(scene::Scene, nonlinearities::Simulation73.AbstractPopulationP;
+        pop_names,
+        title="Population activation functions")
+    layout = GridLayout()
+
+    nonl_xs, nonl_ys = _auto_range(nonlinearities |> Simulation73.array)
+    @show nonl_xs
+    ax = layout[1,1] = LAxis(scene, xlabel="input (a.u.)", ylabel="pop. activity (proportion)", title=title)
+    @show parse(Colorant, ax.attributes[:backgroundcolor][])
+    colors = distinguishable_colors(length(pop_names), parse(Colorant, ax.attributes[:backgroundcolor][]), dropseed=true)
+    plots = [lines!(ax, xs, ys, width=3, color=color) 
+        for (xs, ys, color) in zip(nonl_xs, nonl_ys, colors)]
+    legend_names = ["$(pop)" for pop in pop_names]
+    leg = LLegend(scene, plots, legend_names,
+                  tellheight=false, tellwidth=false,
+                  halign=:left, valign=:top, orientation=:vertical)
+    layout[1,1] = leg
+
+    layout
+end
+
--- src/figures/example.jl
+++ src/figures/example.jl
@@ -4,27 +4,27 @@ function figure_example!(scene::Scene, mdb_path::String,
     figure_example!(scene, prototype_name, all_mods, fixing_mods)
 end
 
-function figure_example!(scene::Scene, prototype_name, all_mods, fixing_mods) where NAMES
+function figure_example!(scene::Scene, prototype_name, all_mods, fixing_mods::NamedTuple{NAMES}; title=string(fixing_mods), kwargs...) where NAMES
     fixed_mods = Dict(key => val for (key,val) in pairs(all_mods) if length(val)== 1)
     unfixed_mod_names = [key for (key, val) in pairs(all_mods) if length(val) > 1]
     @assert Set(NAMES) == Set(unfixed_mod_names) "$(Set(NAMES) - Set(unfixed_mod_names)): all varying parameters must be fixed by example, and no extraneous mods are allowed"
 
     # other_opts = Dict() makes sure it saves all frames
     mods = (fixed_mods..., fixing_mods..., save_idxs=nothing, save_on=true)
-    write_modifications!(plots_subdir, mods, unique_id)
+    #write_modifications!(plots_subdir, mods, unique_id)
     prototype = get_prototype(prototype_name)
     (mod_name, exec) = execute_single_modification(prototype, mods)
 
-    layout = exec_heatmap!(scene, exec; clims=(0.0,0.5), title=string(fixing_mods))
-    layout[end+1,2] = LText(scene, string(exec.simulation.global_reduction(exec.solution).propagation), tellwidth=false)
+    layout = exec_heatmap!(scene, exec; colorrange=(0.0,0.5), title=title, kwargs...)
+    # layout[end+1,2] = LText(scene, string(exec.simulation.global_reduction(exec.solution).propagation), tellwidth=false)
 
     return layout
 end
 
 function figure_example(prototype_name, all_mods, fixing_mods;
-        scene_resolution=(800,400))
+        scene_resolution=(800,400), kwargs...)
     scene, layout = layoutscene(resolution=scene_resolution)
-    layout[1,1:2] = figure_example!(scene, prototype_name, all_mods, fixing_mods)
+    layout[1,1:2] = figure_example!(scene, prototype_name, all_mods, fixing_mods; kwargs...)
     fixing_mods_strs = ["$(name)=$(val)" for (name, val) in pairs(fixing_mods)]
     return (scene, layout, join(fixing_mods_strs, "_"))
 end
@@ -41,3 +41,12 @@ function multiple_figure_examples(mdb_path, fixing_mods_list; kwargs...)
     end
 end
 
+function multiple_figure_examples!(scene, mdb_path, fixing_mods_list; kwargs...)
+    prototype_name, all_mods = read_params_from_data_path(mdb_path)
+    map(fixing_mods_list) do fixing_mods
+        fixing_mods_strs = ["$(name)=$(val)" for (name, val) in pairs(fixing_mods)]
+        (scene, figure_example!(scene, prototype_name, all_mods, fixing_mods; kwargs...), join(fixing_mods_strs, "_"))
+    end
+end
+
+export multiple_figure_examples, multiple_figure_examples!
