a1f5f9c291e9736784d68bee1954460e9cbe3d6a
Graham Smith <grahamas@gmail.com>
2021-01-27 15:28:28 -0700

    Nullcline intersection fixes 1. Only count intersections sufficiently separated 2. FIXME does not force intersections to cross, only to be close

--- scripts/nullclines_and_fixedpoints.jl
+++ scripts/nullclines_and_fixedpoints.jl
@@ -2,13 +2,14 @@ using TravelingWaveSimulationsPlotting, TravelingWaveSimulations
 using WilsonCowanModel, Simulation73
 using Dates
 using Contour
+using AxisIndices
 
 session_name = "nullclines_and_fixedpoints"
 session_id = "$(Dates.now())"
 
 metasweep_A_fpath = TravelingWaveSimulations.get_recent_simulation_data_path(joinpath(homedir(), "data", "ring_normed_blocking", "wider_strength_depthreshold_A"))
 
-function calculate_fixedpoints(mdb_path::AbstractString, new_mods::NamedTuple{NAMES}) where NAMES
+function calculate_fixedpoints(mdb_path::AbstractString, new_mods::NamedTuple{NAMES}, dx::T=0.01) where {NAMES, T<:Number}
     prototype_name, saved_mods = read_params_from_data_path(mdb_path)
     @assert NAMES ⊆ keys(saved_mods)
     combined_mods = merge(saved_mods, Dict(pairs(new_mods))) # overwrites saved_mods
@@ -16,34 +17,17 @@ function calculate_fixedpoints(mdb_path::AbstractString, new_mods::NamedTuple{NA
     variable_mod_names, variable_mod_values = zip([(name, values) for (name,values) in pairs(combined_mods) if length(values)!= 1]...)
     n_fixedpoints_arr = NamedAxisArray{variable_mod_names}(zeros(Int, length.(variable_mod_values)...), variable_mod_values...)
 
+    prototype = get_prototype(prototype_name)
+
     map(product(variable_mod_values...)) do mod_values
         fixing_mods = NamedTuple{variable_mod_names}(mod_values)
         # other_opts = Dict() makes sure it saves all frames
         mods = (combined_mods..., fixing_mods..., save_idxs=nothing, save_on=true)
         #write_modifications!(plots_subdir, mods, unique_id)
-        prototype = get_prototype(prototype_name)
         model = prototype(; mods...)
-        setindex!(n_fixedpoints_arr, calculate_fixedpoints(model); fixing_mods...)
+        setindex!(n_fixedpoints_arr, calculate_fixedpoints(model, dx); fixing_mods...)
     end
-    return n_fixedpoints_arr
-end
-
-function calculate_fixedpoints(mdb_path::AbstractString, fixing_mods::Nothing=nothing)
-    prototype_name, all_mods = read_params_from_data_path(mdb_path)
-    
-    fixed_mods = Dict(key => val for (key,val) in pairs(all_mods) if length(val)== 1)
-    DUMMY_fixing_mods = Dict(key => val[1] for (key, val) in pairs(all_mods) if length(val) > 1)
-
-    # other_opts = Dict() makes sure it saves all frames
-    mods = (fixed_mods..., DUMMY_fixing_mods..., save_idxs=nothing, save_on=true)
-
-    #write_modifications!(plots_subdir, mods, unique_id)
-    prototype = get_prototype(prototype_name)
-    model = prototype(; mods...)
-
-    return translate_model_to_nullcline_params(model)
-
-    return calculate_fixedpoints(model)
+    return n_fixedpoints_arr, combined_mods, prototype
 end
 
 using ForwardDiff
@@ -95,13 +79,52 @@ function find_all_roots(fn::Function, (lower, upper)::Tuple{Vector{T}, Vector{T}
     end
 end
 
-function calculate_fixedpoints(model::Union{AbstractModel{T},AbstractSimulation{T}}) where T
-    nullcline_params = translate_model_to_nullcline_params(model)
-    
-    dx = dy = 0.1
-    us = 0.0:dx:1.0
-    vs = 0.0:dy:1.0
-    radius = sqrt(dx^2 + dy^2)
+struct Intersection{PointType,T}
+    identified_points::Vector{PointType}
+    error::T
+end
+struct Intersections{PointType,T}
+    intersections::Vector{Intersection{PointType,T}}
+    error::T
+end
+function add_point!(intersection::Intersection{P}, point::P) where P
+    if any(calc_distance.(intersection.identified_points, Ref(point)) .<= intersection.error)
+        push!(intersection.identified_points, point)
+        return true
+    else 
+        return false
+    end
+end
+function add_putative_intersection!(existing_intersections::Intersections{P}, point::P) where P
+    """
+        Adds point to list of intersections, either:
+            1) as new intersection
+            2) as "same" as existing intersection
+            3) as "same" as multiple existing intersections, combining
+    """
+    intersections_arr = existing_intersections.intersections
+    impinged_intersections = add_point!.(intersections_arr, Ref(point))
+    if sum(impinged_intersections) == 0
+        # make new intersection
+        push!(intersections_arr, Intersection([point], existing_intersections.error))
+    elseif sum(impinged_intersections) > 1
+        # some intersections can be joined
+        keep_idx, dispose_idxs... = findall(impinged_intersections)
+        for dispose_idx in dispose_idxs
+            append!(intersections_arr[keep_idx].identified_points, intersections_arr[dispose_idx].identified_points)
+        end
+        deleteat!(intersections_arr, dispose_idxs)
+    end 
+end
+
+
+function calculate_fixedpoints(model::Union{AbstractModel{T},AbstractSimulation{T}}, phase_dx::T=0.01, phase_dy::T=phase_dx) where {T <: Number}
+    nullcline_params = wcm_nullcline_params(model)
+
+    @assert phase_dx == phase_dy
+    us = 0.0:phase_dx:1.0
+    vs = 0.0:phase_dy:1.0
+    radius = sqrt(phase_dx^2 + phase_dy^2)
     
     dus = [WilsonCowanModel.wcm_du_defn(u, v, nullcline_params) for u in us, v in vs]
     dvs = [WilsonCowanModel.wcm_dv_defn(u, v, nullcline_params) for u in us, v in vs]
@@ -111,13 +134,14 @@ function calculate_fixedpoints(model::Union{AbstractModel{T},AbstractSimulation{
     n_intersections = 0
     for u_line in u_nullclines
         for v_line in v_nullclines
-            n_intersections += count_intersections(u_line, v_line, dx)
+            n_intersections += count_intersections(u_line, v_line, radius)
         end
     end
 
     return n_intersections
 end
 
+calc_distance(p1::P, p2::P) where P = calc_distance((p1,p2))
 calc_distance((p1, p2)) = sqrt(sum((p1 .- p2) .^ 2))
 function count_intersections(line1, line2, radius)
     # 1. find local minima of distance between vertices
@@ -126,11 +150,20 @@ function count_intersections(line1, line2, radius)
     # FIXME there's a faster way to do this
 
     point_pairs = product(line1.vertices, line2.vertices)
-    intersections = findall(calc_distance.(point_pairs) .<= radius)
-    return length(intersections)
+    intersection_idxs = findall(calc_distance.(point_pairs) .<= radius)
+    length(intersection_idxs) == 0 && return 0
+    all_intersections = getindex.(Ref(point_pairs |> collect), intersection_idxs)
+    first_intersection_p1, first_intersection_p2 = all_intersections[begin]
+    unique_intersections = Intersections([Intersection([first_intersection_p1], radius)], radius)
+    add_putative_intersection!(unique_intersections, first_intersection_p2)
+    @assert length(unique_intersections.intersections) == 1
+    for intersection_points in all_intersections
+        add_putative_intersection!.(Ref(unique_intersections), intersection_points)
+    end
+    return length(unique_intersections.intersections)
 end
 
-calculate_fixedpoints(metasweep_A_fpath, (stim_strength=2.0, blocking_θI = 9.,
-    #Aii=125., Aie=125.)
-    Aii=1.0, Aie=15.0)
-)
+@time fp_arr, cmods, proto = calculate_fixedpoints(metasweep_A_fpath, (stim_strength=2.0, blocking_θI = 9.
+    #, Aii=125., Aie=125.
+    #, Aii=1.0, Aie=15.0
+), 0.01);
--- scripts/plot_nonlinearity_and_nullclines.jl
+++ scripts/plot_nonlinearity_and_nullclines.jl
@@ -10,11 +10,11 @@ include("../_drafts/plot_nonlinearity.jl")
 prototype = get_prototype("full_dynamics_blocking")
 sim = prototype(; α=(1.08, 1.15), Aie=0.81, Aei=0.8, firing_θI=0.37, blocking_θI=0.65, save_idxs=nothing, save_on=true, saveat=0.1)
 
-params = HE2018DepParams(sim)
+params = wcm_nullcline_params(sim)
 
-nullcline_sc, nullcline_ly = nullclines(params, 0.001);
+nullcline_sc, nullcline_ly = nullclines(params, 0.01);
 
-nonl_scene, nonl_layout = layout_plot(plot_nonlinearity!, sim);
+# nonl_scene, nonl_layout = layout_plot(plot_nonlinearity!, sim);
 
-exec = execute(sim)
-exec_heatmap(exec)
+# exec = execute(sim)
+# exec_heatmap(exec)
