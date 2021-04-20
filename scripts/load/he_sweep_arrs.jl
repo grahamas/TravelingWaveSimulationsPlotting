33b01de11b71f11b9c0f0dfdb45698825337fe1e
Graham Smith <grahamas@gmail.com>
2021-04-20 10:17:58 -0500

    More flexible sweep_arr generation

--- scripts/load/he_sweep_arrs.jl
+++ scripts/load/he_sweep_arrs.jl
@@ -5,20 +5,30 @@ if !@isdefined(refresh_sweep_arrs)
     refresh_sweep_arrs = false
 end
 
+if !@isdefined(A_sweep_lower_bound)
+    A_sweep_lower_bound = 0.1
+end
+
+if !@isdefined(A_sweep_upper_bound)
+    A_sweep_upper_bound = 1.5
+end
+
+
 @time if !@isdefined(blocking_fp_arr) || !@isdefined(monotonic_fp_arr) || refresh_sweep_arrs
-    A_range = 0.1:0.1:1.5
-    nullcline_sweeping_mods = (Aee=A_range, Aei=A_range, Aie=A_range, Aii=A_range)
+    
     nullcline_static_mods = (
         α=(0.4, 0.7), 
         firing_θI=0.2, blocking_θI=0.5, 
         n_lattice = 2,
         save_idxs=nothing, save_on=false, saveat=0.1
     ) 
-    file, filename = produce_or_load(datadir(), []; 
+    file, filename = produce_or_load(datadir(), Dict("lower" => A_sweep_lower_bound, "upper"=>A_sweep_upper_bound); 
         prefix = "blocking_fp_arr",
         suffix = "bson",
         force = refresh_sweep_arrs
     ) do c
+        A_range = c["lower"]:0.1:c["upper"]
+        nullcline_sweeping_mods = (Aee=A_range, Aei=A_range, Aie=A_range, Aii=A_range)
         blocking_nullcline_static_mods = nullcline_static_mods
         blocking_prototype_name = "full_dynamics_blocking"
         blocking_fp_arr = wcm_sweep_calculate_fixedpoints(
@@ -30,12 +40,14 @@ end
         )
         return @dict(blocking_fp_arr, blocking_nullcline_static_mods, blocking_prototype_name)
     end
-    @unpack blocking_fp_arr, blocking_nullcline_static_mods, blocking_prototype_name = file
-    file, filename = produce_or_load(datadir(), []; 
+    @unpack blocking_fp_arr = file #, blocking_nullcline_static_mods, blocking_prototype_name = file
+    file, filename = produce_or_load(datadir(), Dict("lower" => A_sweep_lower_bound, "upper"=>A_sweep_upper_bound); 
         prefix = "monotonic_fp_arr",
         suffix = "bson",
         force = refresh_sweep_arrs
     ) do c
+        A_range = c["lower"]:0.1:c["upper"]
+        nullcline_sweeping_mods = (Aee=A_range, Aei=A_range, Aie=A_range, Aii=A_range)
         monotonic_nullcline_static_mods = nullcline_static_mods
         monotonic_prototype_name = "full_dynamics_monotonic"
         monotonic_fp_arr = wcm_sweep_calculate_fixedpoints(
@@ -47,7 +59,7 @@ end
         )
         return @dict(monotonic_fp_arr, monotonic_nullcline_static_mods, monotonic_prototype_name)
     end
-    @unpack monotonic_fp_arr, monotonic_nullcline_static_mods, monotonic_prototype_name = file
+    @unpack monotonic_fp_arr = file #, monotonic_nullcline_static_mods, monotonic_prototype_name = file
 end
 
 refresh_sweep_arrs = false
