263baca20875ddbae77a183803bf17b5b42ab61c
Graham Smith <grahamas@gmail.com>
2021-05-24 17:34:23 -0500

    Better nullcline arrow stuff

--- scripts/pharmocology/phase_space_comparisons.jl
+++ scripts/pharmocology/phase_space_comparisons.jl
@@ -24,7 +24,9 @@ let blocking_prototype_name = "full_dynamics_blocking",
     session_id = "$(Dates.now())",
     figure_resolution=(800,800),
     simple_theme = Theme(
-        linewidth = 35.0,
+        Lines = (
+            linewidth=4.0,
+        ),
         fontsize=30,
         Axis = (
             backgroundcolor = :white,
@@ -37,10 +39,14 @@ let blocking_prototype_name = "full_dynamics_blocking",
         ),
         Arrows = (
             arrowsize=10, lengthscale=0.017,
-            linewidth=7,
+            linewidth=2,
             arrowcolor=:black, linecolor=:black,
             colormap=ColorSchemes.Greys_5,
             normalize=false
+        ),
+        Scatter = (
+            markersize=27,
+            strokewidth=1
         )
     ),
     plots_subdir = plotsdir("$(session_name)_$(session_id)"),
@@ -76,140 +82,95 @@ for (sym, prototype_name) ∈ [(:blocking, blocking_prototype_name), (:monotonic
         get_nullcline_params(prototype(; smods..., low_Aie_3sfp...));
         plot_name="nullclines_$(sym)_low_Aie_3sfp_$(low_Aie_3sfp).$(ext_2d)",
         plots_subdir=plots_subdir,
-        arrows_step=arrows_step,
-        #title="ΔAie = $(round(low_Aie_3sfp[:Aie] - vulnerable_3sfp[:Aie], digits=1))"
+        arrows_step=arrows_step
     )
 
-    plot_and_save(plot_nullclines!,
-        get_nullcline_params(prototype(; smods..., low_Aee_3sfp...));
-        plot_name="nullclines_$(sym)_low_Aee_3sfp_$(low_Aee_3sfp).$(ext_2d)",
-        plots_subdir=plots_subdir,
-        arrows_step=arrows_step,
-        #title="ΔAee = $(round(low_Aee_3sfp[:Aee] - vulnerable_3sfp[:Aee], digits=1))"
-    )
+    # plot_and_save(plot_nullclines!,
+    #     get_nullcline_params(prototype(; smods..., low_Aee_3sfp...));
+    #     plot_name="nullclines_$(sym)_low_Aee_3sfp_$(low_Aee_3sfp).$(ext_2d)",
+    #     plots_subdir=plots_subdir,
+    #     arrows_step=arrows_step,
+    #     #title="ΔAee = $(round(low_Aee_3sfp[:Aee] - vulnerable_3sfp[:Aee], digits=1))"
+    # )
 
-    plot_and_save(plot_nullclines!,
-        get_nullcline_params(prototype(; smods..., low_excitation_3sfp...));
-        plot_name="nullclines_$(sym)_low_excitation_3sfp_$(low_excitation_3sfp).$(ext_2d)",
-        plots_subdir=plots_subdir,
-        arrows_step=arrows_step,
-        #title="ΔAee = $(round(low_excitation_3sfp[:Aee] - vulnerable_3sfp[:Aee], digits=1)); ΔAie = $(round(low_excitation_3sfp[:Aie] - vulnerable_3sfp[:Aie], digits=1))"
-    )
+    # plot_and_save(plot_nullclines!,
+    #     get_nullcline_params(prototype(; smods..., low_excitation_3sfp...));
+    #     plot_name="nullclines_$(sym)_low_excitation_3sfp_$(low_excitation_3sfp).$(ext_2d)",
+    #     plots_subdir=plots_subdir,
+    #     arrows_step=arrows_step,
+    #     #title="ΔAee = $(round(low_excitation_3sfp[:Aee] - vulnerable_3sfp[:Aee], digits=1)); ΔAie = $(round(low_excitation_3sfp[:Aie] - vulnerable_3sfp[:Aie], digits=1))"
+    # )
 
-    plot_and_save(plot_nullclines!,
-        get_nullcline_params(prototype(; smods..., high_Aii_3sfp...));
-        plot_name="nullclines_$(sym)_high_Aii_3sfp_$(high_Aii_3sfp).$(ext_2d)",
-        plots_subdir=plots_subdir,
-        arrows_step=arrows_step,
-        #title="ΔAii = $(round(high_Aii_3sfp[:Aii] - vulnerable_3sfp[:Aii], digits=1))"
-    )
+    # plot_and_save(plot_nullclines!,
+    #     get_nullcline_params(prototype(; smods..., high_Aii_3sfp...));
+    #     plot_name="nullclines_$(sym)_high_Aii_3sfp_$(high_Aii_3sfp).$(ext_2d)",
+    #     plots_subdir=plots_subdir,
+    #     arrows_step=arrows_step,
+    #     #title="ΔAii = $(round(high_Aii_3sfp[:Aii] - vulnerable_3sfp[:Aii], digits=1))"
+    # )
 
-    plot_and_save(plot_nullclines!,
-        get_nullcline_params(prototype(; smods..., high_Aei_3sfp...));
-        plot_name="nullclines_$(sym)_high_Aei_3sfp_$(high_Aei_3sfp).$(ext_2d)",
-        plots_subdir=plots_subdir,
-        arrows_step=arrows_step,
-        #title="ΔAei = $(round(high_Aei_3sfp[:Aei] - vulnerable_3sfp[:Aei], digits=1))"
-    )
+    # plot_and_save(plot_nullclines!,
+    #     get_nullcline_params(prototype(; smods..., high_Aei_3sfp...));
+    #     plot_name="nullclines_$(sym)_high_Aei_3sfp_$(high_Aei_3sfp).$(ext_2d)",
+    #     plots_subdir=plots_subdir,
+    #     arrows_step=arrows_step,
+    #     #title="ΔAei = $(round(high_Aei_3sfp[:Aei] - vulnerable_3sfp[:Aei], digits=1))"
+    # )
 
-    plot_and_save(plot_nullclines!,
-        get_nullcline_params(prototype(; smods..., high_inhibition_3sfp...));
-        plot_name="nullclines_$(sym)_high_inhibition_3sfp_$(high_inhibition_3sfp).$(ext_2d)",
-        plots_subdir=plots_subdir,
-        arrows_step=arrows_step,
-        #title="ΔAii = $(round(high_inhibition_3sfp[:Aii] - vulnerable_3sfp[:Aii], digits=1)); ΔAei = $(round(high_inhibition_3sfp[:Aei] - vulnerable_3sfp[:Aei], digits=1))"
-    )
+    # plot_and_save(plot_nullclines!,
+    #     get_nullcline_params(prototype(; smods..., high_inhibition_3sfp...));
+    #     plot_name="nullclines_$(sym)_high_inhibition_3sfp_$(high_inhibition_3sfp).$(ext_2d)",
+    #     plots_subdir=plots_subdir,
+    #     arrows_step=arrows_step,
+    #     #title="ΔAii = $(round(high_inhibition_3sfp[:Aii] - vulnerable_3sfp[:Aii], digits=1)); ΔAei = $(round(high_inhibition_3sfp[:Aei] - vulnerable_3sfp[:Aei], digits=1))"
+    # )
 
-    plot_and_save(plot_nullclines!,
-        get_nullcline_params(prototype(; smods..., vulnerable_3sfp..., firing_θI= θif+Δθ, θE=θef+Δθ));
-        plot_name="nullclines_$(sym)_EI_firing_Δθ_$(Δθ)_3sfp.$(ext_2d)",
-        plots_subdir=plots_subdir,
-        arrows_step=arrows_step,
-        #title="Firing Δθ: $(Δθ)"
-    )
+    # plot_and_save(plot_nullclines!,
+    #     get_nullcline_params(prototype(; smods..., vulnerable_3sfp..., firing_θI= θif+Δθ, θE=θef+Δθ));
+    #     plot_name="nullclines_$(sym)_EI_firing_Δθ_$(Δθ)_3sfp.$(ext_2d)",
+    #     plots_subdir=plots_subdir,
+    #     arrows_step=arrows_step,
+    #     #title="Firing Δθ: $(Δθ)"
+    # )
 
-    Δθ = -0.005
-    plot_and_save(plot_nullclines!,
-        get_nullcline_params(prototype(; smods..., vulnerable_3sfp..., θE=θef+Δθ));
-        plot_name="nullclines_$(sym)_E_firing_Δθ_$(Δθ)_3sfp.$(ext_2d)",
-        plots_subdir=plots_subdir,
-        arrows_step=arrows_step,
-        #title="Firing ΔθE: $(Δθ)"
-    )
+    # Δθ = -0.005
+    # plot_and_save(plot_nullclines!,
+    #     get_nullcline_params(prototype(; smods..., vulnerable_3sfp..., θE=θef+Δθ));
+    #     plot_name="nullclines_$(sym)_E_firing_Δθ_$(Δθ)_3sfp.$(ext_2d)",
+    #     plots_subdir=plots_subdir,
+    #     arrows_step=arrows_step,
+    #     #title="Firing ΔθE: $(Δθ)"
+    # )
 
-    Δθ = -0.025
-    plot_and_save(plot_nullclines!,
-        get_nullcline_params(prototype(; smods..., vulnerable_3sfp..., θE=θef+Δθ));
-        plot_name="nullclines_$(sym)_E_firing_Δθ_$(Δθ)_3sfp.$(ext_2d)",
-        plots_subdir=plots_subdir,
-        arrows_step=arrows_step,
-        #title="Firing ΔθE: $(Δθ)"
-    )
+    # Δθ = -0.025
+    # plot_and_save(plot_nullclines!,
+    #     get_nullcline_params(prototype(; smods..., vulnerable_3sfp..., θE=θef+Δθ));
+    #     plot_name="nullclines_$(sym)_E_firing_Δθ_$(Δθ)_3sfp.$(ext_2d)",
+    #     plots_subdir=plots_subdir,
+    #     arrows_step=arrows_step,
+    #     #title="Firing ΔθE: $(Δθ)"
+    # )
 
-    Δθ = 0.25
-    plot_and_save(plot_nullclines!,
-        get_nullcline_params(prototype(; smods..., vulnerable_3sfp..., blocking_θI=θib+Δθ, firing_θI=θif+Δθ));
-        plot_name="nullclines_$(sym)_I_both_Δθ_$(Δθ)_3sfp.$(ext_2d)",
-        plots_subdir=plots_subdir,
-        arrows_step=arrows_step,
-        #title="Both ΔθI: $(Δθ)"
-    )
+    # Δθ = 0.25
+    # plot_and_save(plot_nullclines!,
+    #     get_nullcline_params(prototype(; smods..., vulnerable_3sfp..., blocking_θI=θib+Δθ, firing_θI=θif+Δθ));
+    #     plot_name="nullclines_$(sym)_I_both_Δθ_$(Δθ)_3sfp.$(ext_2d)",
+    #     plots_subdir=plots_subdir,
+    #     arrows_step=arrows_step,
+    #     #title="Both ΔθI: $(Δθ)"
+    # )
 
-    if prototype_name == blocking_prototype_name
-        Δθ = 0.3
-        plot_and_save(plot_nullclines!,
-            get_nullcline_params(prototype(; smods..., vulnerable_3sfp..., blocking_θI=θib+Δθ));
-            plot_name="nullclines_$(sym)_I_blocking_Δθ_$(Δθ)_3sfp.$(ext_2d)",
-            plots_subdir=plots_subdir,
-            arrows_step=arrows_step,
-            #title="Blocking ΔθI: $(Δθ)"
-        )
-    end
-    
-
-    # θef = 0.125
-    # θif = 0.2
-    # θib = 0.5
-    # for Δθ ∈ 0.05:0.1:0.25
-    #     plot_and_save(plot_nullclines!,
-    #         get_nullcline_params(prototype(; smods..., vulnerable_3sfp..., firing_θI= θif+Δθ, θE=θef+Δθ));
-    #         plot_name="nullclines_$(sym)_EI_firing_Δθ_$(Δθ)_3sfp_$(EI_firing_Δθ_$(Δθ)_3sfp).$(ext_2d)",
-    #         plots_subdir=plots_subdir,
-    #         arrows_step=arrows_step,
-    #         #title="Firing Δθ: $(Δθ)"
-    #     )
-    # end
-
-    # for Δθ ∈ 0.05:0.1:0.25
-    #     plot_and_save(plot_nullclines!,
-    #         get_nullcline_params(prototype(; smods..., vulnerable_3sfp..., θE=θef+Δθ));
-    #         plot_name="nullclines_$(sym)_E_firing_Δθ_$(Δθ)_3sfp_$(E_firing_Δθ_$(Δθ)_3sfp).$(ext_2d)",
-    #         plots_subdir=plots_subdir,
-    #         arrows_step=arrows_step,
-    #         #title="Firing ΔθE: $(Δθ)"
-    #     )
-    # end
-
-    # for Δθ ∈ 0.05:0.1:0.25
-    #     plot_and_save(plot_nullclines!,
-    #         get_nullcline_params(prototype(; smods..., vulnerable_3sfp..., blocking_θI=θib+Δθ, firing_θI=θif+Δθ));
-    #         plot_name="nullclines_$(sym)_I_both_Δθ_$(Δθ)_3sfp_$(I_both_Δθ_$(Δθ)_3sfp).$(ext_2d)",
-    #         plots_subdir=plots_subdir,
-    #         arrows_step=arrows_step,
-    #         #title="Both ΔθI: $(Δθ)"
-    #     )
-    # end
-
-    # for Δθ ∈ 0.1:0.1:0.4
+    # if prototype_name == blocking_prototype_name
+    #     Δθ = 0.3
     #     plot_and_save(plot_nullclines!,
     #         get_nullcline_params(prototype(; smods..., vulnerable_3sfp..., blocking_θI=θib+Δθ));
-    #         plot_name="nullclines_$(sym)_I_blocking_Δθ_$(Δθ)_3sfp_$(I_blocking_Δθ_$(Δθ)_3sfp).$(ext_2d)",
+    #         plot_name="nullclines_$(sym)_I_blocking_Δθ_$(Δθ)_3sfp.$(ext_2d)",
     #         plots_subdir=plots_subdir,
     #         arrows_step=arrows_step,
     #         #title="Blocking ΔθI: $(Δθ)"
     #     )
     # end
-
+    
 
 end # for
 end # with_theme
